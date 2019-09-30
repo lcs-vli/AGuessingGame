51ddcd995d5de12fa609c6ed692caf481505bd1e
Victoria Li <VLi@lcs.on.ca>
2019-09-30 11:16:59 -0400

    Initial Commit

---
+++ AGuessingGame/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AGuessingGame
+//
+//  Created by Li, Muchen on 2019-09-30.
+//  Copyright © 2019 Li, Muchen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
