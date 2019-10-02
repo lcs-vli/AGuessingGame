fcdd6a96b91f0498930ca4e718a42a5dde89daa4
Victoria Li <VLi@lcs.on.ca>
2019-10-02 11:17:48 -0400

    connected the text field to the controller using an outlet

--- AGuessingGame/ViewController.swift
+++ AGuessingGame/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     
     //MARK: Properties - store information
     let targetNumber = Int.random(in: 1...100)
+    @IBOutlet weak var submittedGuess: UITextField!
     
     //MARK: Initializers
     
